9876336742d98a0aabadf5716bd29fa1a99d2b18
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-02-23 23:58:47 +0200

    Abstract Factory info

--- src/main/java/design_patterns/_01_creational/_01_factory_method/_02_static_method_implementation/generic_factory/ExtendedOrderFactory.java
+++ src/main/java/design_patterns/_01_creational/_01_factory_method/_02_static_method_implementation/generic_factory/ExtendedOrderFactory.java
@@ -1,7 +1,6 @@
 package design_patterns._01_creational._01_factory_method._02_static_method_implementation.generic_factory;
 
 import design_patterns._01_creational._01_factory_method._02_static_method_implementation.ExtendedOrder;
-import design_patterns._01_creational._01_factory_method._02_static_method_implementation.SimpleOrder;
 
 public class ExtendedOrderFactory extends AbstractOrderFactory<ExtendedOrder> {
 
