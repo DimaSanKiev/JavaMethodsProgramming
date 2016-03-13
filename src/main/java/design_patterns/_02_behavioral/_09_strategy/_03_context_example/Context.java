c0aa4ab97ccefb365367ee87c06a2d748fd74bc7
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-03-14 01:00:48 +0200

    Strategy context example

---
+++ src/main/java/design_patterns/_02_behavioral/_09_strategy/_03_context_example/Context.java
@@ -0,0 +1,33 @@
+package design_patterns._02_behavioral._09_strategy._03_context_example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Context {
+    public final static int DEFAULT_ALGORITHM = 0;
+    private Map<Integer, Strategy> algorithms = new HashMap<>();
+
+    public Context(Strategy strategy) {
+        // null check
+        algorithms.put(DEFAULT_ALGORITHM, strategy);
+    }
+
+    public Context() {
+        this(new DefaultStrategy());
+    }
+
+    public void registerAlgorithm(int key, Strategy strategy) {
+        if (key != 0) {
+            algorithms.put(key, strategy);
+        }
+    }
+
+    public void registerDefaultAlgorithm(Strategy strategy) {
+        // null check
+        algorithms.put(DEFAULT_ALGORITHM, strategy);
+    }
+
+    public void contextStrategy(int key) {
+        algorithms.get(key).doAlgorithm();
+    }
+}
---
+++ src/main/java/design_patterns/_02_behavioral/_09_strategy/_03_context_example/Strategy.java
@@ -0,0 +1,5 @@
+package design_patterns._02_behavioral._09_strategy._03_context_example;
+
+public interface Strategy {
+    void doAlgorithm();
+}
