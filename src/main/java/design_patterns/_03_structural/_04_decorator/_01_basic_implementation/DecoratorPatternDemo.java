f16f9336f5c4318b92127ac61649a19eeb8953e7
kenneth carmichael <carmichael@kenneths-MacBook-Pro.local>
2016-03-12 20:24:22 -0700

    Add basic implementation of decorator class

---
+++ src/main/java/design_patterns/_03_structural/_04_decorator/_01_basic_implementation/DecoratorPatternDemo.java
@@ -0,0 +1,23 @@
+package design_patterns._03_structural._04_decorator._01_basic_implementation;
+
+/**
+ * Created by carmichael on 2016-03-12.
+ */
+public class DecoratorPatternDemo {
+    public static void main(String[] args) {
+
+        Shape circle = new Circle();
+
+        Shape redCircle = new RedShapeDecorator(new Circle());
+
+        Shape redRectangle = new RedShapeDecorator(new Rectangle());
+        System.out.println("Circle with normal border");
+        circle.draw();
+
+        System.out.println("\nCircle of red border");
+        redCircle.draw();
+
+        System.out.println("\nRectangle of red border");
+        redRectangle.draw();
+    }
+}
---
+++ src/main/java/design_patterns/_03_structural/_04_decorator/_01_basic_implementation/shape.java
@@ -0,0 +1,8 @@
+package design_patterns._03_structural._04_decorator._01_basic_implementation;
+
+/**
+ * Created by carmichael on 2016-03-12.
+ */
+public interface Shape {
+    void draw();
+}
