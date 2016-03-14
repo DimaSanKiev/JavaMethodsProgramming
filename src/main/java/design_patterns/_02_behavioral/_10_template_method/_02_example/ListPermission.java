c8b1577dfcbe56702a46fbc604bf07da747892a3
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-03-15 00:12:26 +0200

    Template Method example

---
+++ src/main/java/design_patterns/_02_behavioral/_10_template_method/_02_example/ListPermission.java
@@ -0,0 +1,5 @@
+package design_patterns._02_behavioral._10_template_method._02_example;
+
+public class ListPermission {
+    // user permissions list organization depending on their role
+}
---
+++ src/main/java/design_patterns/_02_behavioral/_10_template_method/_02_example/User.java
@@ -0,0 +1,4 @@
+package design_patterns._02_behavioral._10_template_method._02_example;
+
+public class User {
+}
