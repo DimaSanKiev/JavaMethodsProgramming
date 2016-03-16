ae29d2d59c4d870bf0d35d2a18238680179c36de
kenneth carmichael <carmichael@kenneths-MacBook-Pro.local>
2016-03-15 18:35:45 -0600

    Add starbuzz coffee example

---
+++ src/main/java/design_patterns/_03_structural/_04_decorator/_02_starbuzz_example/Soy.java
@@ -0,0 +1,22 @@
+package design_patterns._03_structural._04_decorator._02_starbuzz_example;
+
+/**
+ * Created by carmichael on 2016-03-12.
+ */
+public class Soy extends CondimentDecorator{
+    private Beverage beverage;
+
+    public Soy(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Soy";
+    }
+
+    @Override
+    public double cost() {
+        return 0.15 + beverage.cost();
+    }
+}
