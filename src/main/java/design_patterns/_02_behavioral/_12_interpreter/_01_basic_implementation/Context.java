7365b8219988192dfb5c068590bdfcf81464b426
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-03-19 00:08:37 +0200

    Interpreter basic implementation

---
+++ src/main/java/design_patterns/_02_behavioral/_12_interpreter/_01_basic_implementation/Context.java
@@ -0,0 +1,5 @@
+package design_patterns._02_behavioral._12_interpreter._01_basic_implementation;
+
+public class Context {
+    // some code here
+}
---
+++ src/main/java/design_patterns/_02_behavioral/_12_interpreter/_01_basic_implementation/NonTerminalExpression.java
@@ -0,0 +1,10 @@
+package design_patterns._02_behavioral._12_interpreter._01_basic_implementation;
+
+public class NonTerminalExpression implements AbstractExpression {
+    private AbstractExpression expression;
+
+    @Override
+    public void interpret() {
+        // some code here
+    }
+}
