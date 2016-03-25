1047002c66a7b24433ad28edee599229d7e5b128
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-03-25 19:58:18 +0200

    Parametrized Test

---
+++ src/test/java/junit/_08_parametrized/ScholarshipCalculatorTest4.java
@@ -0,0 +1,44 @@
+package junit._08_parametrized;
+
+import junit._03_exceptions_test.NoSuchMarkException;
+import junit._03_exceptions_test.ScholarshipCalculatorImpl3;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ScholarshipCalculatorTest4 {
+    // parameters declaration as fields
+    private int averageMark;
+    private double stepUpCoefficient;
+
+    // public constructor with parameters for fields initialization
+
+    public ScholarshipCalculatorTest4(int averageMark, double stepUpCoefficient) {
+        this.averageMark = averageMark;
+        this.stepUpCoefficient = stepUpCoefficient;
+    }
+
+    // parameters set definition as a collection
+    @Parameterized.Parameters
+    public static Collection<Object[]> stepUpCoefficientTableValues() {
+        return Arrays.asList(new Object[][] {
+                {3, 1.0},
+                {4, 1.3},
+                {5, 1.5}
+        });
+    }
+
+    @Test
+    public void stepUpCoefficientTest() throws NoSuchMarkException {
+        ScholarshipCalculatorImpl3 scholarshipCalculator = new ScholarshipCalculatorImpl3();
+        double expected = this.stepUpCoefficient;
+        double actual = scholarshipCalculator.stepUpCoefficientCalculate(this.averageMark);
+        assertEquals(expected, actual, 0.01);
+    }
+}
