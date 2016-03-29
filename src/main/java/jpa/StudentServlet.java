a9c8b4e6f1213f6154ddb6a01b6f02eb1fdd3840
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-03-30 01:20:00 +0300

    StudentServlet

---
+++ src/main/java/jpa/StudentServlet.java
@@ -0,0 +1,29 @@
+package jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class StudentServlet extends HttpServlet {
+    @PersistenceUnit(unitName = "UniversityDB")
+    private EntityManagerFactory factory;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = factory.createEntityManager();
+        try {
+            List studentList = em.createQuery("SELECT s FROM Student s").getResultList();
+            request.setAttribute("students", studentList);
+            getServletContext().getRequestDispatcher("/jsp/list_students.jsp")
+                    .forward(request, response);
+        } finally {
+            em.close();
+        }
+    }
+}
