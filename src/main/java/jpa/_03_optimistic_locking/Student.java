47b5874f979165a68a39354ad5aec15820da4650
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-03-31 00:12:26 +0300

    JPA NamedQueries

--- src/main/java/jpa/_03_optimistic_locking/Student.java
+++ src/main/java/jpa/_03_optimistic_locking/Student.java
@@ -2,9 +2,19 @@ package jpa._03_optimistic_locking;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
 
+@NamedQueries({
+        @NamedQuery(name = "Student.findAll",
+                query = "SELECT s FROM Student s"),
+        @NamedQuery(name = "Student.findById",
+                query = "SELECT s FROM Student s WHERE s.id = :id"),
+        @NamedQuery(name = "Student.findByFirstName",
+                query = "SELECT s FROM Student s WHERE s.firstName = :firstName"),
+        @NamedQuery(name = "Student.findByLastName",
+                query = "SELECT s FROM Student s WHERE s.lastName = :lastName")
+})
 public class Student implements Serializable {
-    // some code here
 
     private Long id;
 
@@ -16,11 +26,19 @@ public class Student implements Serializable {
     }
 
     private EntityManagerFactory factory;
-    private Student student = new Student();
 
-    public void find() {
+    public void optimisticLockingDemo() {
+        Student student = new Student();
         EntityManager entityManager = factory.createEntityManager();
         entityManager.find(Student.class, student.getId(), LockModeType.PESSIMISTIC_WRITE);
+    }
+
+    public void namedQueriesDemo() {
+        EntityManager entityManager = factory.createEntityManager();
+        String lastName = "testLastName";
+        Query query = entityManager.createNamedQuery("Student.findByLastName");
+        query.setParameter("lastName", lastName);
+        Collection students = query.getResultList();
     }
 
     public Long getId() {
