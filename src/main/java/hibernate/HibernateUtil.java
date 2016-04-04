5c8506b85c72ed0817628b3776f39b97c1f0736e
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-04-05 00:24:10 +0300

    HibernateUtil

---
+++ src/main/java/hibernate/HibernateUtil.java
@@ -0,0 +1,25 @@
+package hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            // create the SessionFactory from hibernate.cfg.xml
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable e) {
+            // make sure to log the exception as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
